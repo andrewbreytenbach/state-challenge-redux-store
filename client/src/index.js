da6c797c759cf420296ba04d5e709fb7b0ba6f7c
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-30 10:27:27 -0400

    modified the index.js file so it used redux instead of react

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configureStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import rootReducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
+// Configure the Redux store
+const store = configureStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
